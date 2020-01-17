08a607cd46c8bf3bbf592eae285bb199fad53989
Cassie Boyd <cassandra.bacon@gmail.com>
2020-01-17 14:16:35 -0600

    first commit

---
+++ classes.py
@@ -0,0 +1,25 @@
+"""
+Create a Pizza type for representing pizzas in Python. Think about some basic properties that would define a pizza's values; things like size, crust type, and toppings would help. Define those in the __init__ method so each instance can have its own specific values for those properties.
+"""
+
+class Pizza:
+    def __init__(self):
+        self.crust = ""
+        self.size = ""
+        self.topping = ""
+        self.sauce = ""
+        self.cheese = ""
+
+"""
+Add a method for interacting with a pizza's toppings, called add_topping.
+"""
+
+def add_topping(self, topping):
+    self.topping = topping
+
+"""
+Add a method for outputting a description of the pizza (sample output below).
+"""
+
+def description():
+    
