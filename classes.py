7dd276d8ba187a014d1c972ba5c18d542c61b29c
Cassie Boyd <cassandra.bacon@gmail.com>
2020-01-20 20:30:44 -0600

    Can handle more than two toppings without looking goofy unless you need an Oxford comma

--- classes.py
+++ classes.py
@@ -13,15 +13,23 @@ class Pizza:
     def print_order(self):
         if len(self.toppings) == 1:
             print(f"I would like a {self.size}-inch, {self.crust} pizza with {''.join(self.toppings)}.")
+
+        # If more than one topping, .join method is used on toppings list 
         elif len(self.toppings) > 1:
-            sentence = f"I would like a {self.size}-inch, {self.crust} pizza with "
-            
-        
-            
+            print(f"I would like a {self.size}-inch, {self.crust} pizza with {', '.join(self.toppings[:-1])} and {self.toppings[-1]}")
+
+# https://stackoverflow.com/questions/44574485/joining-words-together-with-a-comma-and-and/44574565
+            # print("I would like a {}-inch, {} pizza with {} and {}".format(self.size, self.crust,', '.join(self.toppings[:-1]), self.toppings[-1]))
+
+
             
             # print(f"I would like a {self.size}-inch, {self.crust} pizza with {' and '.join(self.toppings)}")
+            # print(*self.toppings, sep = ", ")
             
-            # {*self.toppings, sep = " and "}")
+            # sentence = f"I would like a {self.size}-inch, {self.crust} pizza with "
+            # print(f"I would like a {self.size}-inch, {self.crust} pizza with {' and '.join(self.toppings)}")
+            
+            # {*self.toppings, sep = " and "}
 
 """
 Add a method for interacting with a pizza's toppings, called add_topping.
@@ -41,4 +49,5 @@ meat_lovers.crust = "Deep dish"
 meat_lovers.add_toppings("Pepperoni")
 meat_lovers.add_toppings("Olives")
 meat_lovers.add_toppings("Mushrooms")
+meat_lovers.add_toppings("Pineapple")
 meat_lovers.print_order()  
